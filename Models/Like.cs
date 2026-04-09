0abe793ae75c5c5af2b8373894e36a2c92e28518
Malek <202275145@edu.clg.qc.ca>
2026-04-09 14:30:12 -0400

    Ajoutez des fichiers projet.

---
+++ Models/Like.cs
@@ -0,0 +1,33 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+   public class Like : Record
+   {
+       public int MediaId { get; set; }
+       public int UserId { get; set; }
+
+       public override bool IsValid()
+       {
+         if (DB.Medias.Get(MediaId) == null)
+         {
+            return false;
+
+         }
+         if (DB.Users.Get(UserId) == null)
+         {
+            return false;
+         }
+         if (DB.Likes.ToList().Exists(l => l.MediaId == MediaId && l.UserId == UserId && l.Id != Id))
+         {
+            return false;
+         }
+
+         return true;
+       }
+   }
+}
---
+++ Models/Media.cs
@@ -0,0 +1,58 @@
+﻿using DAL;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+    public enum MediaSortBy { Title, PublishDate, Likes }
+
+    public class Media : Record
+    {
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string YoutubeId { get; set; }
+        public DateTime PublishDate { get; set; } = DateTime.Now;
+
+        public int OwnerId { get; set; } = 1;
+        public bool Shared { get; set; } = true;
+        [JsonIgnore]
+        public User Owner => DB.Users.Get(OwnerId).Copy();
+        public int Likes => DB.Likes.ToList().Where(l => l.MediaId == Id).Count();
+
+        [JsonIgnore]
+        public List<User> LikedByUsers
+        {
+            get
+            {
+                var likesList = DB.Likes.ToList().Where(l => l.MediaId == Id);
+                List<User> users = new List<User>();
+                foreach (Like like in likesList)
+                {
+                    var user = DB.Users.Get(like.UserId);
+                    if (user != null)
+                        users.Add(user);
+                }
+                return users;
+            }
+        }
+
+        public bool IsLikedByConnectedUser()
+        {
+            if (User.ConnectedUser == null) return false;
+            return DB.Likes.ToList().Exists(l => l.MediaId == Id && l.UserId == User.ConnectedUser.Id);
+        }
+
+      public override bool IsValid()
+        {
+            if (!HasRequiredLength(Title, 1)) return false;
+            if (!HasRequiredLength(Category, 1)) return false;
+            if (!HasRequiredLength(Description, 1)) return false;
+            if (DB.Medias.ToList().Where(m => m.YoutubeId == YoutubeId && m.Id != Id).Any()) return false;
+            return true;
+        }
+    }
+}
